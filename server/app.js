b17ffe55f50e395d1726c2e6a2bc0931b549fada
Lihuel <l1iperione@gmail.com>
2026-03-03 12:45:34 +0100

    Add routing

--- server/app.js
+++ server/app.js
@@ -6,24 +6,35 @@ const PORT = 3000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
+app.get('/', (req, res) => {
+  res.redirect('/login');
+});
+
 app.get('/home', (req, res) =>{
   res.sendFile(path.join(__dirname, '../dist/home.html'));
 });
 
-app.get('/', (req, res) => {
-  res.redirect('/login');
+app.get('/chat', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/chat.html'));
 });
+
+app.get('/group', (req,res)=>{
+  res.sendFile(path.join(__dirname, '../dist/group.html'));
+});
+
 app.get('/login', (req, res)=>{
   res.sendFile(path.join(__dirname, '../dist/login.html'));
+});
+app.get('/register', (req,res)=>{
+  res.sendFile(üath.join(__dirname, '../dist/register.html'));
 });
 
 app.get('/test', (req, res) => {
   res.json({message: "test works"});
 });
 
-app.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/chat.html'));
-});
+
+
 
 app.listen(PORT, () =>{
   console.log(`server running on http://localhost:${PORT}`);
