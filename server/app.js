fd609445bb206b43cefcb35cc17ef416517235c6
ilse547 <lilieco@pm.me>
2026-03-09 11:15:45 +0100

    Add Logger middleware

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const {logger} = require('./middleware/logger');
 
 const PORT = 3000;
 
+
+app.use(logger);
 app.use(express.static(path.join(__dirname, '../dist')));
+
+
 
 app.get('/', (req, res) => {
   res.redirect('/login');
