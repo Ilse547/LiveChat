204ea95a759f9d8a2c0f70b6f265181f6a237d0d
ilse547 <lilieco@pm.me>
2026-03-16 18:04:59 +0100

    Add MinLength and MaxLength to user model

--- server/models/user.js
+++ server/models/user.js
@@ -5,10 +5,14 @@ const userSchema = new mongoose.Schema
     type: String,
     required: true,
     unique: true,
+    minlength:3,
+    maxlength:10
   },
-    password:{
-        type: String,
-        required:true,
-    }
+  password:{
+    type: String,
+    required:true,
+    minlength:8,
+    maxlength:16
+  }
 });
 module.exports = mongoose.model('Usermodel', userSchema);
