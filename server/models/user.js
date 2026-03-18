f5c0c31a954ba6db9db71d5ce94b2e3c2829ccaa
ilse547 <lilieco@pm.me>
2026-03-18 23:02:13 +0100

    Edit User model

--- server/models/user.js
+++ server/models/user.js
@@ -3,17 +3,21 @@ const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema
 ({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    minlength:3,
-    maxlength:10
+  username : {
+    type : String,
+    required : true,
+    unique : true,
+    minlength : 3,
+    maxlength : 10
   },
-  password:{
-    type: String,
-    required:true,
-    minlength:8,
+  password : {
+    type : String,
+    required : true,
+    minlength : 8,
+  },
+  admin : {
+    type : Boolean,
+    default : false
   }
 });
 
