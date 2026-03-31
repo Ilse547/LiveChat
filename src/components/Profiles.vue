8bb4321a989a7263f6fa09396e000f3f921b5fe9
lihuel <lihuel@bazzite.fritz.box>
2026-03-31 22:19:06 +0200

    Create Profiles page

---
+++ src/components/Profiles.vue
@@ -0,0 +1,22 @@
+  <template>
+    <div class="ProfileDiv">
+      <h1>Hello</h1>
+      <p>{{ username }}'s Profile</p>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'Profile',
+    data() {
+      return {
+        username: ''
+      }
+    },
+    mounted() {
+      const urlParts = window.location.pathname.split('/');
+      this.username = urlParts[urlParts.length - 1];
+      document.title = `${this.username}'s Profile`;
+    }
+  }
+  </script>
---
+++ src/pages/profiles.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import Profiles from '../components/Profiles.vue';
+import '../styles/style.css';
+import '../icon/icon.png';
+
+createApp(Profiles).mount('#app');
