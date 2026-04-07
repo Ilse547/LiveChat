35dffb8fc84a3a98604ef89ab1d469501856ae5b
ilse547 <lilieco@pm.me>
2026-04-07 14:42:23 +0200

    Add logout button

--- src/components/Profiles.vue
+++ src/components/Profiles.vue
@@ -2,6 +2,7 @@
     <div class="ProfileDiv">
       <h1>Hello</h1>
       <p>{{ username }}'s Profile</p>
+      <button @click="Logout" style="color: red;"> Logout </button>
     </div>
   </template>
 
@@ -17,6 +18,12 @@
       const urlParts = window.location.pathname.split('/');
       this.username = urlParts[urlParts.length - 1];
       document.title = `${this.username}'s Profile`;
+    },
+    methods: {
+      Logout() {
+        localStorage.removeItem('token');
+        window.location.href="/home"
+      }
     }
   }
   </script>
