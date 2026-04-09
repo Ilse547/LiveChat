baddecbe65b96af4f6379c9616d52bcc17385cc2
ilse547 <lilieco@pm.me>
2026-04-09 19:25:36 +0200

    Styling of button

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -16,7 +16,7 @@
       <div class="MessageDiv">
         <p v-for="message in messages" :key="message.id">
           <strong>{{ message.username }}</strong>: {{ message.text }}
-          <button v-if="message.username === username" @click="DeleteMessage(message.id)" class="DeleteButton"> Del your message</button>
+          <button v-if="message.username === username" @click="DeleteMessage(message.id)" class="DeleteMessageButton"> Del your message</button>
         </p>
       </div>
 
