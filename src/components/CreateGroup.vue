7a09f39653ad4b0ce5b2f85faf65772f75aed291
ilse547 <lilieco@pm.me>
2026-04-10 11:43:08 +0200

    Change Form

--- src/components/CreateGroup.vue
+++ src/components/CreateGroup.vue
@@ -7,10 +7,11 @@
         <p> Group name :</p>
         <input id="GroupNameInput" class="TextInput" type="text" placeholder="Input the groupname">
         <p> Participants :</p>
-        <input id="ParticipantInput" class="TextInput" type="text" placeholder="Input the Username">
-        <input id="ParticipantInput" class="TextInput" type="text" placeholder="Input the Username">
-        <input id="ParticipantInput" class="TextInput" type="text" placeholder="Input the Username">
-        <input id="ParticipantInput" class="TextInput" type="text" placeholder="Input the Username">
+
+        <div v-for="(participant, index) in participants" :key="index" style="display: flex; gap: 8px; width:100%;">
+          <input v-model="participants[index]" class="TextInput" type="text" placeholder="Write username of Participant">
+          <button class="InputButton" @click="CheckParticipant(index)">x</button>
+        </div>
 
         <button class="InputButton" @click="CreateGroup">Create Group</button>
       </div>
@@ -19,11 +20,37 @@
   </div>
 </template>
     
-  <script>
-  export default {
-    name: 'CreateGroup',
-    mounted() {
-      document.title = 'Create Group'
+<script>
+export default {
+  name: 'CreateGroup',
+  data() {
+    return {
+      participants: ['', '', '', ''],
+      verified: []
+    };
+  },
+  mounted() {
+    document.title = 'Create Group';
+  },
+  methods: {
+    async CheckParticipant(index) {
+      const username = this.participants[index].trim();
+      if(!username) return;
+      try {
+        const response = await fetch(`/user/exists/${username}`);
+        const data = await response.json();
+        if(data.exists) {
+
+          this.verified.push(index);
+          this.participants.splice(index, 1);
+        } else {
+          alert(`Ùser doesnt exist`);
+        }
+      } catch (err){
+        console.error('Error while check user', err);
+        alert('There was an error while checking if the user exists');
+      }
     }
   }
-  </script>
+};
+</script>
