15c78c38e1aecea3eeecd4c964dcf3eff484e83c
ilse547 <lilieco@pm.me>
2026-04-13 11:57:43 +0200

    Add creategroup logic

--- src/components/CreateGroup.vue
+++ src/components/CreateGroup.vue
@@ -5,7 +5,7 @@
         
       <div class="CreateGroup">   
         <p> Group name :</p>
-        <input id="GroupNameInput" class="TextInput" type="text" placeholder="Input the groupname">
+        <input v-model="GroupName" id="GroupNameInput" class="TextInput" type="text" placeholder="Input the groupname">
         <p> Participants :</p>
 
         <div v-for="(participant, index) in participants" :key="index" style="display: flex; gap: 8px; width:100%;">
@@ -50,7 +50,46 @@ export default {
         console.error('Error while check user', err);
         alert('There was an error while checking if the user exists');
       }
-    }
+    },
+    async CreateGroup() {
+      if(!this.GroupName.trim()){
+        alert('Please write a groupname');
+        return;
+      }
+      if(this.verified.length === 0){
+        alert('Please add at least one participants');
+        return;
+      }
+
+      
+      const token = localStorage.getItem('token');
+      if(!token) {
+        window.location.href = '/login';
+        return;
+      }
+
+      try {
+        const response = await fetch('/creategroup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+          body: JSON.stringify({
+            GroupName: this.GroupName.trim(),
+            Participants: this.verified
+          })
+        });
+        const data = await response.json();
+        if(response.ok) {
+          alert('Group was created');
+          window.location.href = '/chat';
+        }else { alert(data.message); }
+      } catch (err) {
+        console.error('Error creating group', err);
+        alert('There was an error creating grooup');
+      }
+    },
   }
 };
 </script>
