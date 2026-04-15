72e3ad41d10b5a3a50c6b73f4899136d3b9adef7
ilse547 <lilieco@pm.me>
2026-04-15 17:50:51 +0200

    Fix the import problem

--- src/pages/group.js
+++ src/pages/group.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import Group from '../components/Chat.vue';
+import Group from '../components/Group.vue';
 import '../styles/style.css';
 import '../icon/icon.png';
 createApp(Group).mount('#app');
