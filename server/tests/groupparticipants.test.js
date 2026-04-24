0642f960660462372fcff7cfce75545b9eff935f
ilse547 <lilieco@pm.me>
2026-04-24 12:31:34 +0200

    Create First Test

--- server/tests/groupparticipants.test.js
+++ server/tests/groupparticipants.test.js
@@ -12,3 +12,29 @@ jest.mock('../models/user', () => {
 	MockUserModel.findOne = jest.fn();
 	return MockUserModel;
 });
+
+jest.mock('mongoose', ()=> ({
+	connect: jest.fn().mockResolvedValue(true)
+}));
+
+const app require('../app');
+const GroupModel = require('../models/group');
+
+function CreateToken(username) {
+	return jwt.sign({username, id: 'FakeId', admin: false}, process.env.JWT_KEY || 'SecretKeyTest');
+}
+
+describe('GET /groupinfo/:groupname', () => {
+	it('should return 200 if user is participant of a group', async () =>{
+		GroupModel.findOne.mockResolvedValue({
+			GroupName: 'testgroup',
+			Participants: ['John', 'Bob', 'Willian']
+		});
+		const token = CreateToken('William');
+		const res = await request(app)
+			.get('/groupinfo/testgroup')
+			.set('Authoriźation', `Bearer ${token}`);
+		expect(res.status).toBe(200);
+		expect(res.body.group.GroupName).toBe('testgroup');
+	});
+})
