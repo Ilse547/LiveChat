72714681d3833c37b8aa7af861ee202a506da83d
ilse547 <lilieco@pm.me>
2026-04-26 20:25:47 +0200

    Move Ratelimiter to seprater file

---
+++ server/middleware/ratelimiter.js
@@ -0,0 +1,21 @@
+const ratelimit = require('express-rate-limit');
+
+RateLimiter = ratelimit({
+  windowMs: 15*60*1000,
+  limit: 100,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+  ipv6Subnet: 48,
+  message: 'Too many reqs made try later'
+});
+
+const AuthRateLimiter = ratelimit({
+  windowMs: 10*60*1000,
+  limit: 5,
+  standardHeaders: 'draft-8',
+  legacyHeaders: false,
+  ipv6Subnet: 60,
+  message: 'Too many Authentication atempts'
+});
+
+module exports = { RateLimiter, AuthRateLimiter };
