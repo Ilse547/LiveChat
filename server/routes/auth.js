e7437ec7c7955a938e4b6c71e22a36bd2ba1b796
ilse547 <lilieco@pm.me>
2026-04-26 21:13:21 +0200

    Fix typo on line 6

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Usermodel = require('../models/user');
 const jwt = require('jsonwebtoken');
-cost VerifyToken = require('../middleware/verifytoken');
+const { VerifyToken } = require('../middleware/verifytoken');
 const JWT_KEY = process.env.JWT_KEY;
 
 //LOGIN LOGIC
