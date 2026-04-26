1802144c2395f4ef05231df2ebc10a9f0694a2f7
ilse547 <lilieco@pm.me>
2026-04-26 21:23:02 +0200

    Change file placement

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -9,31 +9,31 @@ router.get('/', (req, res) => {
 });
 
 router.get('/home', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../dist/home.html'));
+  res.sendFile(path.join(__dirname, '../../dist/home.html'));
 });
 
 router.get('/chat', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/chat.html'));
+  res.sendFile(path.join(__dirname, '../../dist/chat.html'));
 });
 
 router.get('/login', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../dist/login.html'));
+  res.sendFile(path.join(__dirname, '../../dist/login.html'));
 });
 
 router.get('/register', (req,res)=>{
-  res.sendFile(path.join(__dirname, '../dist/register.html'));
+  res.sendFile(path.join(__dirname, '../../dist/register.html'));
 });
 
 router.get('/creategroup',  (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/creategroup.html'));
+    res.sendFile(path.join(__dirname, '../../dist/creategroup.html'));
 });
 
 router.get('/group/:groupname', (req,res)=>{
-  res.sendFile(path.join(__dirname, '../dist/group.html'));
+  res.sendFile(path.join(__dirname, '../../dist/group.html'));
 });
 
 router.get('/user/:username', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/profiles.html'));
+  res.sendFile(path.join(__dirname, '../../dist/profiles.html'));
 });
 
 router.get('/user/exists/:username', async (req, res) => {
@@ -52,5 +52,7 @@ router.get('/test', (req, res) => {
 });
 
 router.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/favicon.ico'));
+  res.sendFile(path.join(__dirname, '../../dist/favicon.ico'));
 });
+
+module.exports = router;
