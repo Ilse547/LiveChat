b0aeb2ea71a2e45bb463135342d6449171ef8a62
lihuel <lihuel@bazzite.fritz.box>
2026-04-28 13:32:14 +0300

    Create send confirmation email logic

--- server/service/email.js
+++ server/service/email.js
@@ -8,3 +8,17 @@ const transporter = nodemailer.createTransport({
 		pass: process.env.EMAIL_PASS
 	}
 });
+
+const SendConfirmationEmail = async (ToEmail, username) => {
+	await transporter.sendMail({
+		from: `"LiveChat" <${process.env.EMAIL_USER}`,
+		to: ToEmail,
+		subject: 'Livechat confirmation email',
+		html: `
+			<h1>This si a confirmation email to ${username} </h1>
+			<p>Account created :)</p>
+			`
+	});
+};
+
+module.exports = { SendConfirmationEmail };
