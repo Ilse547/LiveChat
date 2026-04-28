e627d0a22e6d4a9a712d4c918420a925194869f8
lihuel <lihuel@bazzite.fritz.box>
2026-04-28 13:28:11 +0300

    Create trasnporter

--- server/service/email.js
+++ server/service/email.js
@@ -1,2 +1,10 @@
 //nodemailer
 const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS
+	}
+});
