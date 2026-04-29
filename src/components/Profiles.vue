fc936d94c8001777cb62c9e8ed13758449400366
ilse547 <lilieco@pm.me>
2026-04-29 13:18:15 +0100

    add email and Admin status

--- src/components/Profiles.vue
+++ src/components/Profiles.vue
@@ -1,30 +1,33 @@
-  <template>
-    <div class="Description">
-      <h1>Hello {{ username }}</h1>
-      <p> Admin status: </p>
-      <button class="InputButton" @click="ResetPassword" style="background-color: #ff6b6b;"> Reset password </button>
-      <button class="InputButton" @click="Logout" style="background-color: #ff6b6b;"> Logout </button>
-    </div>
-  </template>
+    <template>
+      <div class="Description">
+        <h1>Hello {{ username }}</h1>
+        <p>Email: {{ email }}</p>
+        <p> Admin status: {{ admin}} </p>
+        <button class="InputButton" @click="ResetPassword" style="background-color: #ff6b6b;"> Reset password </button>
+        <button class="InputButton" @click="Logout" style="background-color: #ff6b6b;"> Logout </button>
+      </div>
+    </template>
 
-  <script>
-  export default {
-    name: 'Profile',
-    data() {
-      return {
-        username: ''
-      }
-    },
-    mounted() {
-      const urlParts = window.location.pathname.split('/');
-      this.username = urlParts[urlParts.length - 1];
-      document.title = `${this.username}'s Profile`;
-    },
-    methods: {
-      Logout() {
-        localStorage.removeItem('token');
-        window.location.href="/home"
+    <script>
+    export default {
+      name: 'Profile',
+      data() {
+        return {
+          username: '',
+          email: '',
+          admin: false,
+        }
+      },
+      mounted() {
+        const urlParts = window.location.pathname.split('/');
+        this.username = urlParts[urlParts.length - 1];
+        document.title = `${this.username}'s Profile`;
+      },
+      methods: {
+        Logout() {
+          localStorage.removeItem('token');
+          window.location.href="/home"
+        }
       }
     }
-  }
-  </script>
+    </script>
