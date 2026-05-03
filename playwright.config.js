b5d81e2c937f0e2ba0053037df25063c3931e492
ilse547 <lilieco@pm.me>
2026-05-03 11:29:06 +0200

    Update filepath

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-  testDir: './server/tests/E2E',
+  testDir: './server/tests/e2e',
   use: {
     baseURL: 'http://localhost:3000',
   },
