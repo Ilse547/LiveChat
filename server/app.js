6a8e16b299282f01b4a5e2e39c97db17b35a0011
ilse547 <lilieco@pm.me>
2026-05-05 13:37:31 +0200

    Uninstall mongo-sanitize

--- server/app.js
+++ server/app.js
@@ -43,7 +43,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 
 app.use(express.json({ limit: '50kb'}));
-app.use(mongosanitize());
 app.use(express.urlencoded({ extended : true }));
 app.use(logger);
 app.use(helmet({
