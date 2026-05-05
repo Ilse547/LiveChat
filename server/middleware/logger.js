d55d189424415fc56600e98f1a06695a0d5531da
ilse547 <lilieco@pm.me>
2026-05-05 13:34:25 +0200

    Write logs to file

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,3 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const LogDir = path.join(__dirname, '../../logs');
+if(!fs.existsSync(LogDir)) fs.mkdirSync(LogDir, { recursive: true });
+const LogStream = fs.createWriteStream(path.join(LogDir, 'access.log'), { flags: 'a' });
+
 function logger(req, res, next) {
   const ip =
     req.ip ||
@@ -7,6 +14,14 @@ function logger(req, res, next) {
 
   const now = new Date().toUTCString();
   console.log(`[${now}] ${ip} ${req.method} ${req.originalUrl}`);
+
+  res.on('finish', () => {
+    const user = req.user?.username || 'guest';
+    const entry = `[${now}] ${ip} ${req.method} ${req.originalUrl} ${res.statusCode} user:${user}\n`;
+    process.stdout.write(entry);
+    LogStream.write(entry);
+  });
+
   next();
 }
 
