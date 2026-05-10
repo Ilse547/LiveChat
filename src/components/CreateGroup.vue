6cc14632cbd6d9a1366a50365fd5c0086408d755
ilse547 <lilieco@pm.me>
2026-05-10 11:48:52 +0200

    Add Aria-label

--- src/components/CreateGroup.vue
+++ src/components/CreateGroup.vue
@@ -5,15 +5,15 @@
         
       <div class="CreateGroup">   
         <p> Group name :</p>
-        <input v-model="GroupName" id="GroupNameInput" class="TextInput" type="text" placeholder="Input the groupname">
+        <input v-model="GroupName" id="GroupNameInput" class="TextInput" type="text" placeholder="Input the groupname" aria-label="Group Name Input">
         <p> Participants :</p>
 
         <div v-for="(participant, index) in participants" :key="index" style="display: flex; gap: 8px; width:100%;">
-          <input v-model="participants[index]" class="TextInput" type="text" placeholder="Write username of Participant">
-          <button class="InputButton" @click="CheckParticipant(index)">x</button>
+          <input v-model="participants[index]" class="TextInput" type="text" placeholder="Write username of Participant" aria-label="Input participant">
+          <button class="InputButton" @click="CheckParticipant(index)"aria-label="Check Participant">x</button>
         </div>
 
-        <button class="InputButton" @click="CreateGroup">Create Group</button>
+        <button class="InputButton" @click="CreateGroup" aria-label="Create Group Button">Create Group</button>
       </div>
 
     </main>
