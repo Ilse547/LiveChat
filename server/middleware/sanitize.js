91aa1099bb4537afdc2c2626a66d929a29aa809b
ilse547 <lilieco@pm.me>
2026-05-11 18:35:58 +0200

    Fix sanitize no longer interferes wiht email

--- server/middleware/sanitize.js
+++ server/middleware/sanitize.js
@@ -1,10 +1,18 @@
 function sanitize(req, res, next) {
 	if (req.body) {
-		req.body = JSON.parse(
-			JSON.stringify(req.body)
-			.replace(/\$|\./g, '_')
-		);
+		req.body = sanitizeObject(req.body);
 	}
 	next();
 }
+
+function sanitizeObject(obj) {
+	if (typeof obj !== 'object'|| obj === null ) return obj;
+	const clean ={};
+	for (const key of Object.keys(obj)) {
+		const cleanKey = key.replace(/^\$/, '_');
+		clean[cleanKey] = sanitizeObject(obj[key]);
+	}
+	return clean;
+}
+
 module.exports = { sanitize };
